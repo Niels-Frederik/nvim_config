c9f947bcbf50dc3ca1afdf679f6c9c64e009b365
nieb <niels.frederik.brinck@gmail.com>
2023-04-25 20:31:05 +0200

    added comment on keyword option

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -37,6 +37,6 @@ vim.opt.guifont = "monospace:h17"               -- the font used in graphical ne
 vim.opt.shortmess:append "c"
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"
-vim.cmd [[set iskeyword+=-]]
-vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
+vim.cmd [[set iskeyword+=-]]                    -- Treats '+=-' as a single word (some-variable is a single word instead of 2)
+vim.cmd [[set formatoptions-=cro]]              -- TODO: this doesn't seem to work
 
