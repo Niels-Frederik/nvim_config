0dc7061a18bc396908f5491cd24c2dd82a8b9687
nieb <niels.frederik.brinck@gmail.com>
2023-04-28 22:06:33 +0200

    added telescope, keymappings to open and media extensions to view media files in telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,6 +45,8 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
   use "lunarvim/colorschemes"
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-telescope/telescope-media-files.nvim"
 
   -- completion
   use "hrsh7th/nvim-cmp" -- The completion plugin
